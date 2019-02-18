f02474ce036183bc6251333c5e96067a2a934c39
Ahiezer Rivas <ahiezerrivas@gmail.com>
2019-02-18 16:40:02 -0300

    Minuto 25

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+
+//importing routes
+ const indexRoutes = require('./routes/index');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}))
+
+//routes
+app.use('/', indexRoutes);
+//stating the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
