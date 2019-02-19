b680bcfd5c9020a19c6b70e5bb538c90ab289e81
Ahiezer <ahiezerrivas@gmail.com>
2019-02-18 22:04:35 -0400

    min 53

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+router.post('/add', (req,res) => {
+    console.log(new Task(req.body));
+    
+    res.send('received');
+})
 
 module.exports = router;
